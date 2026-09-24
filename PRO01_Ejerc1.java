66d01d8cdfe2437c6d024a34a5bfe3d0c2f2b8b0
DefectiveRussian <deni.x9@gmail.com>
2026-09-24 12:05:46 +0100

    task1final added

---
+++ PRO01_Ejerc1.java
@@ -0,0 +1,11 @@
+public class PRO01_Ejerc1 {
+    public enum Choice {M, F};
+    public static void main(String[] args) {
+        
+        final int MAX_NUMBER = 5000;
+        boolean driversLicense = true;
+        String fullName = "Deniel Khamidullin Barabanshchikov";
+        Choice sex = Choice.M; 
+        double earthToJupiter = 777908928;
+    }
+}
