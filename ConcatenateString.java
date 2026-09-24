408154664d88855a9c3d04c400e6f2f2cce9d5f4
DefectiveRussian <deni.x9@gmail.com>
2026-09-24 11:31:01 +0100

    concat

---
+++ ConcatenateString.java
@@ -0,0 +1,10 @@
+public class ConcatenateString {
+    public static void main(String[] args) {
+        String text1 = "Hola";
+        char letra1 = 'M';
+
+        String concat = text1 + letra1;
+
+        System.out.println("Resulting string: " + concat);
+    }
+}
