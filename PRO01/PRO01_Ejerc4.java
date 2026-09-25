19900d41e41a687a809b7930d4552a9ebce64d87
DefectiveRussian <deni.x9@gmail.com>
2026-09-25 13:56:25 +0100

    name change

--- PRO01/PRO01_Ejerc4.java
+++ PRO01/PRO01_Ejerc4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PRO1_Ejerc4 { //give monthly salary, say if it matches minimum interpersonal wage (1221 euro/month)
+public class PRO01_Ejerc4 { //give monthly salary, say if it matches minimum interpersonal wage (1221 euro/month)
     public static void main(String[] args) {
         final int minWage = 1221;
         int takenWage;
