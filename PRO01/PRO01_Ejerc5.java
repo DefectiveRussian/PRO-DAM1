b92d026d539d928735f1d8589ca245df05be298a
DefectiveRussian <deni.x9@gmail.com>
2026-09-25 13:07:16 +0100

    added ejerc5

---
+++ PRO01/PRO01_Ejerc5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PRO01_Ejerc5 {
+    public static void main(String[] args) {
+        int takenNumber;
+        int convertToMinutes;
+        int convertToHours;
+        int convertToDays;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce the amount of seconds");
+        takenNumber = sc.nextInt();
+        convertToMinutes = takenNumber / 60;
+        convertToHours = convertToMinutes / 60;
+        convertToDays = convertToHours / 24;
+        if (takenNumber == 0 || takenNumber > 0)
+            System.out.println("Given seconds: " + takenNumber + "\nIn Minutes: " + convertToMinutes + " In Hours: " + convertToHours + " In Days: " + convertToDays);
+        else
+            System.out.println("Negative values are not allowed");
+        sc.close();
+    }
+}
